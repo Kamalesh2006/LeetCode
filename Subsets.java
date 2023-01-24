350d10865d3cbad8700c8554a8a9d2c2694e5a8a
Kamalesh2006 <kamalesh20062001@gmail.com>
2023-01-24 17:35:58 +0530

    Subsets

---
+++ Subsets.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    
+    private static void subset(List<List<Integer>> result, List<Integer> l, List<Integer> dt){
+        List<Integer> tempL = new ArrayList<>(l);
+        result.add(tempL);
+        if(dt.size()==0){
+            return;
+        }
+        List<Integer> tempDt = new ArrayList<>(dt);
+        while(tempDt.size()!=0){
+            tempL.add(tempDt.remove(0));
+            subset(result,tempL,tempDt);
+            tempL.remove(tempL.size()-1);
+        }    
+
+    }
+    /**
+     * 
+     * @param nums
+     * @return List<List<Integer>>
+     */
+    public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> dt=new ArrayList<>();
+        //decision tree
+        for(int i:nums){
+            dt.add(i);
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        subset(result, new ArrayList<Integer>(),dt);
+        return result;
+    }
+}
