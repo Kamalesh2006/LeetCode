8d3e576c9a22bd5fadc3d75a0dfff0f2b8838ea2
Kamalesh2006 <kamalesh20062001@gmail.com>
2023-01-26 18:16:50 +0530

    added comments to the CombinationSum

--- CombinationSum.java
+++ CombinationSum.java
@@ -1,6 +1,14 @@
 import java.util.*;
 
 class CombinationSum {
+    /***
+     * 
+     * @param possible List<Integer> type which stores possible combinations of candidates
+     * @param index of int, index of candidates array
+     * @param candidates int array
+     * @param target int
+     * @param result of List<List<Integer>> which stores list
+     */
     void combinations(List<Integer> possible,int index, int[] candidates, int target, List<List<Integer>> result){
         if(index==candidates.length){
             if(target==0){
@@ -16,6 +24,7 @@ class CombinationSum {
         combinations(possible,index+1,candidates,target,result);
         
     }
+    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         combinations(new ArrayList<>(), 0, candidates, target, result);
--- CombinationSum2.java
+++ CombinationSum2.java
@@ -1,11 +1,20 @@
 import java.util.*;
 
 public class CombinationSum2 {
-    private void combinations(List<Integer> l, int[] candidates, int index, int target, List<List<Integer>> result)
+
+    /**
+     * 
+     * @param possible List<Integer> type which stores possible combinations of candidates
+     * @param candidates int array
+     * @param index of int, index of candidates array
+     * @param target
+     * @param result of List<List<Integer>> which stores list
+     */
+    private void combinations(List<Integer> possible, int[] candidates, int index, int target, List<List<Integer>> result)
     {
         if(index==candidates.length){
             if(target==0){
-                List<Integer> tempL = new ArrayList<>(l);
+                List<Integer> tempL = new ArrayList<>(possible);
                 Collections.sort(tempL);
                 if(!result.contains(tempL))
                     result.add(tempL);
@@ -13,15 +22,15 @@ public class CombinationSum2 {
             return;
         }
         else if(candidates[index]<=target){
-            l.add(candidates[index]);
-            combinations(l,candidates,index+1,target-candidates[index],result);
-            l.remove(l.size()-1);
+            possible.add(candidates[index]);
+            combinations(possible,candidates,index+1,target-candidates[index],result);
+            possible.remove(possible.size()-1);
             
         }
         while(index<candidates.length-1 && candidates[index]==candidates[index+1]){
             index++;
         }
-        combinations(l,candidates,index+1,target,result);
+        combinations(possible,candidates,index+1,target,result);
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
